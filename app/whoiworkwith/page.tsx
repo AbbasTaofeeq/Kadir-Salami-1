f0eb5e049727bc00c8a1fba2ee7af22aff8b3125
AbbasTaofeeq <Tofeeq001@outlook.com>
2026-02-04 20:46:42 +0100

    Problemsolved

---
+++ app/whoiworkwith/page.tsx
@@ -0,0 +1,10 @@
+import WhoIWorkWith from "@/src/components/Who-I-Work-With";
+
+
+export default function OverviewPage() {
+    return (
+        <header>
+            <WhoIWorkWith />
+        </header>
+    );
+}
